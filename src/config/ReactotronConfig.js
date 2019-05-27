13e49243e6c906a201bd70f14583716f8d5eb34f
luciano tavernard <ltms00@hotmail.com>
2019-05-27 20:23:10 -0300

    Concluído o primeiro desafio do módulo de native do bootcamp

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,12 @@
 import Reactotron from 'reactotron-react-native';
 
-const tron = Reactotron.configure()
-  .useReactNative()
-  .connect();
+if (__DEV__) {
+  const tron = Reactotron
+    .configure()
+    .useReactNative()
+    .connect();
 
-tron.clear();
+  tron.clear();
 
-console.tron = tron;
+  console.tron = tron;
+}
